423b093a32463fcb905ef055b7708dee5d8c8886
www42 <thomas.jaekel@gfn.de>
2021-05-28 17:17:14 +0200

    Add demo managed identities

---
+++ tj/demo-managed-ID/ARM.ps1
@@ -0,0 +1,17 @@
+# Request OAuth 2.0 token
+#
+$Uri      = "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F"
+$Header   = @{"Metadata" = "true"}
+$response = Invoke-RestMethod -Headers $Header -Method GET -Proxy $null -Uri $Uri
+$token    = $response.access_token
+
+
+
+# Enumerate Resources via ARM Api
+# Cf.  https://docs.microsoft.com/en-us/rest/api/resources/resources/listbyresourcegroup
+#
+$rgId    = "/subscriptions/26994ff8-a16e-48ed-9eca-8597519aaa5c/resourceGroups/Web-RG"
+$uri     = "https://management.azure.com$rgId/resources?api-version=2021-04-01"
+$Header  = @{"authorization" = "Bearer $token"}
+$response = Invoke-RestMethod -Method Get -Uri $uri -Headers $Header
+$response.value
---
+++ tj/demo-managed-ID/PowerShell.ps1
@@ -0,0 +1,7 @@
+Get-AzContext
+
+# Login-AzAccount -Credential ...
+
+Add-AzAccount -Identity
+
+New-AzRouteTable -Name Table1 -ResourceGroupName Web-RG -Location westeurope 
