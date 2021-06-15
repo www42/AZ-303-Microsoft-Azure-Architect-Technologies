ae7fcc76e987e7a788b31afd8ab2f9bb6990b067
www42 <thomas.jaekel@gfn.training>
2021-06-15 15:09:42 +0200

    Add minor modification to demo managed id scripts

--- tj/demo-managed-ID/PowerShell.ps1
+++ tj/demo-managed-ID/PowerShell.ps1
@@ -1,7 +1,9 @@
+$RgName = 'Web-RG'
+
 Get-AzContext
 
 # Login-AzAccount -Credential ...
 
 Add-AzAccount -Identity
 
-New-AzRouteTable -Name Table1 -ResourceGroupName Web-RG -Location westeurope 
+New-AzRouteTable -Name Table1 -ResourceGroupName $RgName -Location westeurope 
